884262f3b3823b3f19f4852fe57726df3651ef85
rajula <rajula801@gmail.com>
2019-10-13 06:46:02 -0400

    This new project

---
+++ MasterOrigami/src/main/java/com/ori/MasterOrigamiApplication.java
@@ -0,0 +1,13 @@
+package com.ori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterOrigamiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterOrigamiApplication.class, args);
+	}
+
+}
