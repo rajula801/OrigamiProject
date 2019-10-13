b0452ee5cba9a5ba466f13830b596416338c9368
rajula <rajula801@gmail.com>
2019-10-13 07:07:04 -0400

    added new method

--- MasterOrigami/src/main/java/com/ori/controller/MasterController.java
+++ MasterOrigami/src/main/java/com/ori/controller/MasterController.java
@@ -1,5 +1,6 @@
 package com.ori.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +11,10 @@ public class MasterController {
 public String hello() {
 	
 	return "Welcome to Master Origami";
+}
+@RequestMapping("/name/{message}")
+public String getName(@PathVariable("message") String m) {
+	
+	return "your Message is :"+m;
 }
 }
