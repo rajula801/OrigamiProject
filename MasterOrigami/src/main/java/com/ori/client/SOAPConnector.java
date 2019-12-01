3baea1b569d5ba30685289b09e8292e49729cd0c
rajula <rajula801@gmail.com>
2019-12-01 18:27:08 -0500

    This is first change in VC

--- MasterOrigami/src/main/java/com/ori/client/SOAPConnector.java
+++ MasterOrigami/src/main/java/com/ori/client/SOAPConnector.java
@@ -5,6 +5,7 @@ import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 public class SOAPConnector extends WebServiceGatewaySupport {
 	 
     public Object callWebService(String url, Object request){
+        //this is the client soap class
         return getWebServiceTemplate().marshalSendAndReceive(url, request);
     }
 }
